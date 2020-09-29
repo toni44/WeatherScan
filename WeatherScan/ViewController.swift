d20ddb144df91b4451eb79d0087869df9e8ef738
toni_jarvinen <toni44@gmail.com>
2020-09-29 18:45:23 +0300

    Initial Commit

---
+++ WeatherScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherScan
+//
+//  Created by Toni Järvinen on 29.9.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
